7e6755a10d640509ecaeebdae9f76f27ebcfdc42
k-kamada <kamadak0404@gmail.com>
2019-12-08 00:27:51 +0900

    uibuild

---
+++ build/precache-manifest.b37aae69c12c07ea661be14774a92bf4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78e5f2f2b9a0a0ba25bbc021b9898403",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2629a6c47e9c69706e71",
+    "url": "/static/css/main.d24de726.chunk.css"
+  },
+  {
+    "revision": "bc91d37d1b4e857e9ea4",
+    "url": "/static/js/2.baafe9e2.chunk.js"
+  },
+  {
+    "revision": "2629a6c47e9c69706e71",
+    "url": "/static/js/main.a47e3b1f.chunk.js"
+  },
+  {
+    "revision": "d92e7acfcbfa197868a5",
+    "url": "/static/js/runtime-main.d36c9b34.js"
+  }
+]);
