3593c9ceadfa182b439f86ec420ffe4ab72d5a0f
k-kamada <kamadak0404@gmail.com>
2019-12-09 12:29:37 +0900

    Done exercise 3.13: Phonebook database, step1

--- index.js
+++ index.js
@@ -1,52 +1,28 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-
 const dateformat = require('dateformat');
 const morgan = require('morgan');
 const cors = require('cors');
+
+const Person = require('./models/person');
 
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
 app.use(cors());
 app.use(express.static('build'))
 
-let persons = [
-  {
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: 1,
-  },
-  {
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: 2,
-  },
-  {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: 3,
-  },
-  {
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-    id: 4,
-  },
-];
-
 app.get('/api/persons', (req, res) => {
-  res.send(persons);
+  Person.find({}).then(persons => {
+    res.json(persons.map(person => person.toJSON()));
+  });
 });
 
 app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find(person => person.id === id);
-
-  if (person) {
-    res.json(person);
-  } else {
-    res.status('404').end();
-  }
+  Person.findById(req.params.id).then(person => {
+    res.json(person.toJSON());
+  });
 });
 
 app.get('/info', (req, res) => {
@@ -97,7 +73,7 @@ const unknownEndpoint = (req, res) => {
 
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on Port ${PORT}.`);
 });
--- mongo.js
+++ mongo.js
@@ -12,7 +12,7 @@ const password = process.argv[2];
 const dbName = 'phonebook';
 const url = `mongodb+srv://fullstack:${password}@cluster0-hllsn.mongodb.net/${dbName}?retryWrites=true&w=majority`;
 
-mongoose.connect(url, {useNewUrlParser: true});
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
 
 const personSchema = new mongoose.Schema({
   name: String,
